fe5669def8af1b69d30bc826411823269d5007c8
Vinícios Costa <dev.vinicios@gmail.com>
2025-06-24 12:09:53 -0300

    feat: configure hero ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import AppRoutes from './routes/app-routes.tsx'
+import { HeroUIProvider } from '@heroui/react'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AppRoutes />
+      <HeroUIProvider>
+        <AppRoutes />
+      </HeroUIProvider>
     </BrowserRouter>
   </StrictMode>
 )
